80da1502d7ad0b928620287e05ce3d75ae528e5d
StenAntonov <sten.antonov@gmail.com>
2021-12-18 04:12:26 +0200

    added to blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,26 +1,20 @@
 const Blog = () => {
     return (
-        <section className="video-blog">
-            <h2 className="video-blog-heading">Video Blog:</h2>
-            <div className="carousel-wrapper">
-                <span id="item-2"></span>
-                <span id="item-3"></span>
-                <span id="item-1"></span>
-                <div className="carousel-item item-1">
-                    <a href="#item-3" className="arrow-prev arrow"></a>
-                   <a href="#item-2" className="arrow-next arrow"></a>
-               </div>
-
-                <div className="carousel-item item-2">
-                    <a href="#item-1" className="arrow-prev arrow"></a>
-                   <a href="#item-3" className="arrow-next arrow"></a>
-               </div>
-
-                <div className="carousel-item item-3">
-                    <a href="#item-2" className="arrow-prev arrow"></a>
-                   <a href="#item-1" className="arrow-next arrow"></a>
-               </div>
-            </div>
+        <section className="second">
+            <article className="img">
+                <img src="/images/mind if I smoke.jpg" alt="smoker" className="image" />
+            </article>
+            <article className="second-content">
+                <article className="second-content-headings">
+                    <h1 className="second-content-heading-first">Why social smoking can be just as bad for you as daily smoking </h1>
+                </article>
+                <p className="second-content-text-first">“Everything in moderation.” </p>
+                <p className="second-content-text-second">It’s a common justification made for behaviors that may fall outside the realm of healthy. Whether it’s a drink or two or indulging in a favorite dessert, consuming small quantities, rather than abject abstinence, is a more palatable and acceptable option for most people.</p>
+                <p className="second-content-text-third">The less-is-more approach may be sound when applied to many aspects of our frenzied daily lives, but when it comes to smoking, the same rationale cannot apply.</p>
+                <article className="link">
+                    <a className="link" href="https://theconversation.com/why-social-smoking-can-be-just-as-bad-for-you-as-daily-smoking-79968">Read more...</a>
+                </article>
+            </article>
         </section>
     );
 };
